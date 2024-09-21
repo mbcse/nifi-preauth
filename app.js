c052f76d1584d53cfa2393fa456f104db5bfcf69
mbcse <mbcse50@gmail.com>
2024-09-21 12:58:48 +0800

    Added deauth

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var cors = require('cors')
 var app = express();
 
 app.use(logger('dev'));
+
+const timeoutWindow = 180000
+
 app.use(cors({
     origin: '*', // Replace with your frontend domain
     methods: ['GET', 'POST'],
@@ -22,6 +25,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+let timeoutIds = {
+}
+
+setInterval(() => {
+    console.log('Timeouts:', timeoutIds);
+},5000)
+
 app.post('/api/preauth', (req, res) => {
     const { clientmac, clientip } = req.body;
 
@@ -30,15 +41,15 @@ app.post('/api/preauth', (req, res) => {
     }
 
     // Preauthorize using the MAC address
-    exec(`sudo /usr/bin/ndsctl auth ${clientip}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Preauth error: ${error}`);
-            console.log(error)
-            return res.status(500).json({ message: 'Preauthorization failed.' });
-        }
-        console.log(`Preauthorized client: ${clientip}`);
+
+    const success = authorize(clientip)
+    if(success){
         res.status(200).json({ message: 'Preauthorization successful.' });
-    });
+    }else{
+        res.status(500).json({ message: 'Preauthorization failed.' });
+    }   
+
+
 });
 
 /**
@@ -53,15 +64,96 @@ app.post('/api/unauth', (req, res) => {
         return res.status(400).json({ message: 'Client Ip address is required.' });
     }
 
-    // Unauthorize the client using the MAC address
-    exec(`sudo /usr/bin/ndsctl deauth ${clientip}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Unauth error: ${error}`);
-            return res.status(500).json({ message: 'Unauthorization failed.' });
-        }
-        console.log(`Unauthorized client: ${clientmac}`);
+    const success = deauthroize(clientip)
+
+    if(success){
         res.status(200).json({ message: 'Unauthorization successful.' });
-    });
+    }else
+    {
+        res.status(500).json({ message: 'Unauthorization failed.' });
+    }
+   
 });
+
+
+app.post('/api/verified', (req, res) => {
+    console.log(req)
+    const { clientip } = req.body;
+
+    if (!clientip) {
+        return res.status(400).json({ message: 'Client Ip address is required.' });
+    }
+
+    clearTimeout(timeoutIds[clientip]);
+    delete timeoutIds[clientip];
+
+    res.status(200).json({ message: 'Verified successful.' });
+   
+});
+
+
+const authorize = (clientip, res) => {
+    try{
+
+        if(!Object.hasOwn(timeoutIds, clientip)){
+            exec(`sudo /usr/bin/ndsctl auth ${clientip}`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Preauth error: ${error}`);
+                    console.log(error)
+                    return false;
+                }
+                console.log(`Preauthorized client: ${clientip}`);
+
+                timeoutIds[clientip] = setTimeout(() => {
+                    deauthroize(clientip)
+                }, timeoutWindow);
+
+                return true;
+            });
+
+            
+
+        }else{
+            clearTimeout(timeoutIds[clientip]);
+            timeoutIds[clientip] = setTimeout(() => {
+                deauthroize(clientip)
+            }, timeoutWindow);
+            return true
+        }
+
+
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'Preauthorization failed.' });
+        return false;
+    }
+}
+
+
+const deauthroize = (clientip, res) => {
+
+     try {
+        // Unauthorize the client using the MAC address
+        exec(`sudo /usr/bin/ndsctl deauth ${clientip}`, (error, stdout, stderr) => {
+           if (error) {
+               console.error(`Unauth error: ${error}`);
+               return false
+           }
+           console.log(`Unauthorized client: ${clientip}`);
+           clearTimeout(timeoutIds[clientip]);
+           delete timeoutIds[clientip];
+           return true
+       });
+       
+     } catch (error) {
+        console.log(error)
+        res.status(200).json({ message: 'Unauthorization successful.' });
+        if(Object.hasOwn(timeoutIds, clientip)){
+            return false
+        }else{
+            return true
+        }
+     }
+}
 
 module.exports = app;
